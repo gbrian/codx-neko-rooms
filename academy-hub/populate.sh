94539f39adc370ab956e203c4d81c3c812182763
gbrian <gustavo.brian@gmail.com>
2022-11-03 12:14:18 +0000

    Fix academy-hub

---
+++ academy-hub/populate.sh
@@ -0,0 +1,17 @@
+source ../.env
+echo "Pushing images"
+
+function build_image {
+    cd $1
+    IMAGE_NAME=${PWD##*/}
+    echo "Building image $IMAGE_NAME"
+    TAG="${ACADEMY_HUB_DOMAIN}/codx/${IMAGE_NAME}:latest"
+    docker build -t $TAG .
+    docker push $TAG
+}
+
+docker login -u $ACADEMY_HUB_USER -p $ACADEMY_HUB_PASSWORD https://${ACADEMY_HUB_DOMAIN}
+
+for d in */ ; do
+    build_image $d
+done
