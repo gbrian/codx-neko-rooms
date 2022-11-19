3756ad57728d859d0fe944c3ac82c8f161171c7a
gbrian <gustavo.brian@gmail.com>
2022-11-19 10:33:28 +0000

    Some more...

--- academy-hub/codx-room/apps/jupyter-lab.sh
+++ academy-hub/codx-room/apps/jupyter-lab.sh
@@ -1,7 +1,13 @@
 bash $HOME/apps/python.sh
 pip install jupyterlab
-$HOME/.local/bin/jupyter notebook stop 8888
+
+rm .jupyter/jupyter_notebook_config.py || true
+
+# Fix config
 $HOME/.local/bin/jupyter notebook --generate-config
 sed -i "s/\# c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '0.0.0.0'/" .jupyter/jupyter_notebook_config.py
-$HOME/.local/bin/jupyter-lab
+sed -i "s/\# c.NotebookApp.token = '<generated>'/c.NotebookApp.token = ''/" .jupyter/jupyter_notebook_config.py
+sed -i "s/\# c.NotebookApp.allow_origin = ''/c.NotebookApp.allow_origin = '*'/" .jupyter/jupyter_notebook_config.py
+# Register service
+sudo bash $CODX_APPS/register-app.sh "jupyter-lab" "$HOME/.local/bin/jupyter-lab"
 
--- academy-hub/test-hub.sh
+++ academy-hub/test-hub.sh
@@ -2,11 +2,22 @@ source ../.env
 
 docker pull alpine
 
+touch testFile
 docker build -t test:latest -f Dockerfile.test .
 
+
 TAG="$ACADEMY_HUB_DOMAIN/codx/test:latest"
+echo "Tagging image as $TAG"
 docker tag test:latest $TAG
 
+echo "Login"
 docker login -u $ACADEMY_HUB_USER -p $ACADEMY_HUB_PASSWORD https://${ACADEMY_HUB_DOMAIN}
+echo "Push"
 docker push $TAG
+echo "Delete image and pull"
+docker image rm $TAG
 docker pull $TAG
+
+echo "Clean"
+docker image rm test:latest
+rm testFile
