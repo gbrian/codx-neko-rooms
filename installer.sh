730d76305e408b804b543c6b4fb8e57bc0334b41
gbrian <gustavo.brian@gmail.com>
2022-12-22 11:12:50 +0000

    damm...

--- installer.sh
+++ installer.sh
@@ -8,6 +8,7 @@ echo "Clone repo"
 git clone https://github.com/gbrian/codx-neko-rooms.git
 cd codx-neko-rooms
 # Populate neko-rooms
+git submodule init neko-rooms
 git submodule update neko-rooms
 
 echo "Install docker"
