c14d5abd45c1c2880eefa57c5ced56afc0da9aa3
gbrian <gustavo.brian@gmail.com>
2022-12-22 11:13:58 +0000

    Let's see....

--- installer.sh
+++ installer.sh
@@ -8,8 +8,7 @@ echo "Clone repo"
 git clone https://github.com/gbrian/codx-neko-rooms.git
 cd codx-neko-rooms
 # Populate neko-rooms
-git submodule init neko-rooms
-git submodule update neko-rooms
+git submodule update --init neko-rooms
 
 echo "Install docker"
 bash academy-hub/codx-room/apps/docker.sh
