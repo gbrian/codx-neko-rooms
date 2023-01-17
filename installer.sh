68cc6f413b4de7d0542b2b0c362cc23e4cb14cdd
gbrian <gustavo.brian@gmail.com>
2023-01-17 06:24:10 +0000

    Enable storage

--- installer.sh
+++ installer.sh
@@ -15,5 +15,14 @@ git submodule update --init neko-rooms
 echo "Install docker"
 bash academy-hub/codx-room/apps/docker.sh
 
+echo "Data folder"
+mkdir -p /root/codx-neko-rooms/data
+
 echo "Install neko-rooms $@"
 bash install.sh $@
+
+# TODO:....
+echo "Register provider"
+#curl -X POST https://api-codx.meetnav.com/api/cloud-providers/register-neko-rooms \
+#   -H 'Content-Type: application/json' \
+#   -d '{"ip":"my_login","user":"admin","pwd":"my_password","token":"TOOOKKKEENNN"}'
